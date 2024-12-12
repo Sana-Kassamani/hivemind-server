b555a65db1e233f8a4448971eb0a01d28f7975f5
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-12 14:36:54 +0200

    Nestjs <> Add - user schema w/out user type for now

---
+++ src/users/schema/user.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ObjectId } from 'mongoose';
+
+@Schema()
+export class User {
+  _id: ObjectId;
+
+  @Prop({ unique: true, required: true })
+  username: string;
+
+  @Prop({ unique: true, required: true })
+  email: string;
+
+  @Prop({ required: true, select: false })
+  password: string;
+
+  @Prop({ required: true })
+  userType: Enumerator;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
