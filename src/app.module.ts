9dc293fda79f7414e3d10aaf9d739781456d1fe1
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-12 17:55:46 +0200

    Nestjs <> Add - apiaries module to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { ApiariesModule } from './apiaries/apiaries.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_CONNECT),
     UsersModule,
+    ApiariesModule,
   ],
   controllers: [],
   providers: [],
