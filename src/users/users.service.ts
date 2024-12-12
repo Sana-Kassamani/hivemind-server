3ac5426af9475400b489e47f56547bb5c41725c0
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-12 15:14:34 +0200

    Nestjs <> Create - usersService class to define createUser method

---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schema/user.schema';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  findAll() {}
+
+  createUser(createUserDto: CreateUserDto) {
+    const newUser = new this.userModel(createUserDto);
+    return newUser.save();
+  }
+}
