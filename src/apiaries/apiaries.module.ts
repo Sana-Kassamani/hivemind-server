6dd7ddfa1e0f05e8a5ec80cb2aed92ec4c81142a
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-14 20:17:05 +0200

    Nestjs <> Edit - apiary module to inject hives service and use hives controller

--- src/apiaries/apiaries.module.ts
+++ src/apiaries/apiaries.module.ts
@@ -3,6 +3,8 @@ import { ApiariesService } from './apiaries.service';
 import { ApiariesController } from './apiaries.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Apiary, ApiarySchema } from './schema/apiary.schema';
+import { HivesService } from 'src/hives/hives.service';
+import { HivesController } from './hives.controller';
 
 @Module({
   imports: [
@@ -13,7 +15,7 @@ import { Apiary, ApiarySchema } from './schema/apiary.schema';
       },
     ]),
   ],
-  controllers: [ApiariesController],
-  providers: [ApiariesService],
+  controllers: [ApiariesController, HivesController],
+  providers: [ApiariesService, HivesService],
 })
 export class ApiariesModule {}
