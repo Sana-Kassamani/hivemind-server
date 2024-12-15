4d4f6c0a735a6d2d0692814ad7fd54325f8b6359
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-15 21:48:33 +0200

    Nestjs <> Edit - user module to add discriminator

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,13 +4,26 @@ import { Mongoose } from 'mongoose';
 import { User, UserSchema } from './schema/user.schema';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UserTypesModule } from 'src/user-types/user-types.module';
+import {
+  UserType,
+  UserTypesSchema,
+} from 'src/user-types/schema/user-types.schema';
+import { Owner, OwnerSchema } from './schema/owner.schema';
+import { Beekeeper, BeekeeperSchema } from './schema/beekeeper.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       {
         name: User.name,
-        schema: UserSchema,
+        useFactory: () => {
+          const schema = UserSchema;
+          schema.discriminator(Owner.name, OwnerSchema);
+          schema.discriminator(Beekeeper.name, BeekeeperSchema);
+
+          return schema;
+        },
       },
     ]),
   ],
