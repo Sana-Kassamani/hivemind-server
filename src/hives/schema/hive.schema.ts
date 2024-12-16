48df6bccb75b19022a762f2d51dba4f3254ec7ee
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-16 16:06:49 +0200

    Nestjs <> Edit - hives schema to add iotdetails and media arrays

--- src/hives/schema/hive.schema.ts
+++ src/hives/schema/hive.schema.ts
@@ -1,11 +1,19 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { ObjectId } from 'mongoose';
+import { ObjectId, Types } from 'mongoose';
+import {
+  HiveDetails,
+  HiveDetailsSchema,
+} from 'src/hive-details/schema/hive-details.schema';
+import {
+  HiveMedia,
+  HiveMediaSchema,
+} from 'src/hive-media/schema/hive-media.schema';
 
 @Schema()
 export class Hive {
-  _id: ObjectId;
+  _id: Types.ObjectId;
 
-  @Prop({ unique: true, required: true })
+  @Prop({ type: String, unique: true, required: true })
   label: string;
 
   @Prop({ required: true, type: Number })
@@ -16,11 +24,15 @@ export class Hive {
 
   @Prop({ type: Date, required: false })
   lastHarvestDate: Date;
-  // newHive: import('mongoose').Types.ObjectId;
 
-  //   details:[]
-  //   images:[]
-  //   audios:[]
+  @Prop({ type: [HiveDetailsSchema], default: [] })
+  iotDetails: HiveDetails[];
+
+  @Prop({ type: [HiveMediaSchema], default: [] })
+  images: HiveMedia[];
+
+  @Prop({ type: [HiveMediaSchema], default: [] })
+  audios: HiveMedia[];
 }
 
 export const HiveSchema = SchemaFactory.createForClass(Hive);
