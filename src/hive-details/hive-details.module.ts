d71eaa39acc3428a90b826986e77bbb7c185f150
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-19 17:54:19 +0200

    Nestjs <> Add - apiaries model to hiveDetails module

--- src/hive-details/hive-details.module.ts
+++ src/hive-details/hive-details.module.ts
@@ -4,14 +4,17 @@ import { HiveDetailsController } from './hive-details.controller';
 import { HivesModule } from 'src/hives/hives.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Hive, HiveSchema } from 'src/hives/schema/hive.schema';
+import { ApiariesModule } from 'src/apiaries/apiaries.module';
+import { Apiary, ApiarySchema } from 'src/apiaries/schema/apiary.schema';
 
 @Module({
   imports: [
+    ApiariesModule,
     HivesModule,
     MongooseModule.forFeature([
       {
-        name: Hive.name,
-        schema: HiveSchema,
+        name: Apiary.name,
+        schema: ApiarySchema,
       },
     ]),
   ],
