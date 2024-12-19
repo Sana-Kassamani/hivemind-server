aaa7428857f18e07b6ed1ea4437e012ae3197272
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-19 17:50:47 +0200

    Nestjs <> Import - apiaries model to hives module

--- src/hives/hives.module.ts
+++ src/hives/hives.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { HivesService } from './hives.service';
+import { ApiariesModule } from 'src/apiaries/apiaries.module';
+import { ApiariesService } from 'src/apiaries/apiaries.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Apiary, ApiarySchema } from 'src/apiaries/schema/apiary.schema';
+import { HivesController } from './hives.controller';
 
 @Module({
-  controllers: [],
+  imports: [
+    ApiariesModule,
+    MongooseModule.forFeature([
+      {
+        name: Apiary.name,
+        schema: ApiarySchema,
+      },
+    ]),
+  ],
+  controllers: [HivesController],
   providers: [HivesService],
   exports: [HivesService],
 })
