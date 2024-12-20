8d2f259659b7db01aab1403a9625869e882762d6
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-20 17:30:26 +0200

    Nestjs <> Add - Auth Service: validateUser method in addition to login to sign jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,31 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/schema/user.schema';
+import { mongo } from 'mongoose';
 
+type ModifiedUser = Omit<User, 'password'>;
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-  async login(loginDto: LoginDto) {
+  async validateUser(loginDto: LoginDto) {
     const user = await this.usersService.getUserByUsername(loginDto.username);
     //TODO compare bcrypt
     if (user?.password !== loginDto.password)
       throw new UnauthorizedException('Invalid Credentials');
-    const modifiedUser = (({ password, ...rest }) => rest)(user.toObject());
+    const modifiedUser: ModifiedUser = (({ password, ...rest }) => rest)(
+      user.toObject(),
+    );
     return modifiedUser;
+  }
+  async login(loginDto: LoginDto) {
+    const user: ModifiedUser = await this.validateUser(loginDto);
+    const token = await this.jwtService.signAsync({ userId: user._id });
+    return { user: user, token: token };
   }
 }
