26301f923fdb291efd4a70a749337e656eda55f4
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-21 19:59:40 +0200

    Nestjs <> Add - apiaries model to Task Module

--- src/core/tasks/tasks.module.ts
+++ src/core/tasks/tasks.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
+import { ApiariesModule } from '../apiaries/apiaries.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Apiary, ApiarySchema } from '../apiaries/schema/apiary.schema';
 
-@Module({})
+@Module({
+  imports: [
+    ApiariesModule,
+    MongooseModule.forFeature([
+      {
+        name: Apiary.name,
+        schema: ApiarySchema,
+      },
+    ]),
+  ],
+  providers: [TasksService],
+  controllers: [TasksController],
+})
 export class TasksModule {}
