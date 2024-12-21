f4432805f5ef853bc79ccf31b6bc676489248fac
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-21 19:26:13 +0200

    Nestjs <> Edit - Auth guard to allow public endpoints directly

--- src/auth/guards/authentication.guard.ts
+++ src/auth/guards/authentication.guard.ts
@@ -4,7 +4,9 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 export type ReqUser = {
   userId: string;
@@ -12,8 +14,18 @@ export type ReqUser = {
 };
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
   async canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
     const request = context.switchToHttp().getRequest();
     const authHeader = request.headers.authorization; // Bearer <token>
     if (!authHeader) throw new UnauthorizedException();
