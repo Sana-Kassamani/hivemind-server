072895e2a187265c326e08a030dbe11a91014295
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-21 14:53:34 +0200

    Nestjs <> Add - Auth controller: signup method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,10 +7,13 @@ import {
   Post,
   Request,
   UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { AuthGuard } from './guards/auth.guard';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -20,6 +23,13 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
+  }
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  @UsePipes(new ValidationPipe())
+  async signup(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.signup(createUserDto);
   }
 
   @UseGuards(AuthGuard)
