89116f38a2c26dcd0eb68cef9e47080f9e23c998
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-22 00:22:43 +0200

    Nestjs <> Add - Task Service : completeTask for tasks of apiary

--- src/core/tasks/tasks.service.ts
+++ src/core/tasks/tasks.service.ts
@@ -4,6 +4,8 @@ import { Apiary } from '../apiaries/schema/apiary.schema';
 import { Model } from 'mongoose';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './schema/task.schema';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { TaskStatus } from 'src/utils/enums/taskStatus.enum';
 
 @Injectable()
 export class TasksService {
@@ -34,6 +36,28 @@ export class TasksService {
       },
     );
     if (!updatedApiary) throw new NotFoundException('Apiary not found');
+    return updatedApiary;
+  }
+
+  async completeTask(
+    apiaryId: string,
+    taskId: string,
+    updateTaskDto: UpdateTaskDto,
+  ) {
+    const updatedApiary = await this.apiaryModel.findOneAndUpdate(
+      { _id: apiaryId, 'tasks._id': taskId },
+      {
+        $set: {
+          'tasks.$.status': TaskStatus.Done, // Update the status of the matched task
+          'tasks.$.comment': updateTaskDto.comment ?? 'No comment', // Update the comment of the matched task
+        },
+      },
+      {
+        new: true,
+        projection: { tasks: 1 },
+      },
+    );
+    if (!updatedApiary) throw new NotFoundException('Task in apiary not found');
     return updatedApiary;
   }
 }
