fb991d735b5d8d9a244c3ee248a4c86143f2bd35
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-21 19:25:46 +0200

    Nestjs <> Add - Public decorator for login and signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,17 +14,20 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { AuthGuard } from './guards/authentication.guard';
 import { CreateUserDto } from 'src/core/users/dto/create-user.dto';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post()
   @HttpCode(HttpStatus.OK)
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
+  @Public()
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
   @UsePipes(new ValidationPipe())
