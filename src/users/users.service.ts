f4161f30a862fac2d0175705fbed0437947c9861
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-21 13:38:48 +0200

    Nestjs <> Add - User Service : assign userType to Beekeeper by default

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,25 +3,19 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schema/user.schema';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UserType } from 'src/user-types/schema/user-types.schema';
 import { create } from 'domain';
+import { UserType } from 'src/utils/enums/userType.enum';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectModel(User.name) private userModel: Model<User>,
-    @InjectModel(UserType.name) private userTypeModel: Model<UserType>,
-  ) {}
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   getUsers() {
     return this.userModel.find();
   }
 
   async createUser(createUserDto: CreateUserDto) {
-    const userType = await this.userTypeModel.findOne({
-      name: createUserDto.userType ?? 'Beekeeper',
-    });
-    createUserDto.userType = userType.name;
+    !createUserDto.userType && (createUserDto.userType = UserType.BeeKeeper);
     const newUser = new this.userModel(createUserDto);
     return newUser.save();
   }
