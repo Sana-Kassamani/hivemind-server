69a6abba2e789c9b4cf09e0befb80da3b0b9a37d
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-21 13:36:09 +0200

    Nestjs <> Add - Users Schema: pre middleware to hash passwords upon creating user

--- src/users/schema/user.schema.ts
+++ src/users/schema/user.schema.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose, { ObjectId, Types } from 'mongoose';
-
+import mongoose, { HydratedDocument, ObjectId, Types } from 'mongoose';
+import { UserType } from 'src/utils/enums/userType.enum';
+import * as bcrypt from 'bcrypt';
 @Schema({ discriminatorKey: 'userType' })
 export class User {
   _id: Types.ObjectId;
@@ -16,9 +17,26 @@ export class User {
 
   @Prop({
     type: String,
+    enum: UserType,
     required: true,
   })
   userType: string;
 }
 
-export const UserSchema = SchemaFactory.createForClass(User);
+const UserSchema = SchemaFactory.createForClass(User);
+UserSchema.pre('save', async function (next) {
+  const user = this as HydratedDocument<User>;
+  if (!user.isModified('password')) next();
+
+  try {
+    const salt = 10;
+    const hashed = await bcrypt.hash(user.password, salt);
+    console.log('hashed password is ', hashed);
+    user.password = hashed;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+export { UserSchema };
