b1015a0dcc5a2304f98aef0ed853381a3063ab92
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-21 19:11:28 +0200

    Nestjs <> Edit - Hive Contoller : add api endpoints for getAllHives, deleteHive and UpdateHive

--- src/core/hives/hives.controller.ts
+++ src/core/hives/hives.controller.ts
@@ -1,17 +1,26 @@
 import {
   Body,
   Controller,
+  Delete,
+  Get,
   Param,
+  Patch,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { HivesService } from './hives.service';
 import { CreateHiveDto } from './dto/create-hive.dto';
+import { UpdateHiveDto } from './dto/update-hive.dto';
 
 @Controller('hives')
 export class HivesController {
   constructor(private hivesService: HivesService) {}
+
+  @Get(':id')
+  async getAllHives(@Param('id') apiaryId: string) {
+    return this.hivesService.getAllHives(apiaryId);
+  }
 
   @Post(':id')
   @UsePipes(new ValidationPipe())
@@ -20,5 +29,19 @@ export class HivesController {
     @Body() createHiveDto: CreateHiveDto,
   ) {
     return this.hivesService.addHive(apiaryId, createHiveDto);
+  }
+
+  @Patch(':id/:hiveId')
+  updateHive(
+    @Param('id') apiaryId: string,
+    @Param('hiveId') hiveId: string,
+    @Body() updateHiveDto: UpdateHiveDto,
+  ) {
+    return this.hivesService.updateHive(apiaryId, hiveId, updateHiveDto);
+  }
+
+  @Delete(':id/:hiveId')
+  deleteHive(@Param('id') apiaryId: string, @Param('hiveId') hiveId: string) {
+    return this.hivesService.deleteHive(apiaryId, hiveId);
   }
 }
