2b51eb93af26bedcb3840d9e64631c977bf975b8
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-21 20:55:18 +0200

    Nestjs <> Add - Task Service : addTask to tasks of  apiary

--- src/core/tasks/tasks.service.ts
+++ src/core/tasks/tasks.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Apiary } from '../apiaries/schema/apiary.schema';
 import { Model } from 'mongoose';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task } from './schema/task.schema';
 
 @Injectable()
 export class TasksService {
@@ -15,5 +17,23 @@ export class TasksService {
       { tasks: 1 },
     );
     return apiary;
+  }
+
+  async addTask(apiaryId: string, createTaskDto: CreateTaskDto) {
+    const newTask = new Task(createTaskDto);
+    const updatedApiary = await this.apiaryModel.findOneAndUpdate(
+      { _id: apiaryId },
+      {
+        $push: {
+          tasks: newTask,
+        },
+      },
+      {
+        new: true,
+        projection: { tasks: 1 },
+      },
+    );
+    if (!updatedApiary) throw new NotFoundException('Apiary not found');
+    return updatedApiary;
   }
 }
