aa733a8ecee4366faea02a6256c12e7e0cdd49c0
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-14 04:55:20 +0200

    Nestjs <> Add - send push notification in notifictation controller

--- src/core/notifications/notifications.controller.ts
+++ src/core/notifications/notifications.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
+import { CreateNotificationDto } from './dto/create-notification.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(private notificationService: NotificationsService) {}
+
+  @Public()
+  @Post()
+  sendNotification(@Body() pushNotification: CreateNotificationDto) {
+    this.notificationService.sendPush(pushNotification);
+  }
 }
