7a5b8097a574edede474ddfb2b48e142b114681d
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-19 16:20:38 +0200

    Nestjs <> Add - addComment function to tasks.service

--- src/core/tasks/tasks.service.ts
+++ src/core/tasks/tasks.service.ts
@@ -7,7 +7,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Apiary } from '../apiaries/schema/apiary.schema';
 import { Model } from 'mongoose';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task } from './schema/task.schema';
+import { Comment, Task } from './schema/task.schema';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { TaskStatus } from 'src/utils/enums/taskStatus.enum';
 
@@ -16,12 +16,14 @@ export class TasksService {
   constructor(@InjectModel(Apiary.name) private apiaryModel: Model<Apiary>) {}
 
   async getTasks(apiaryId: string) {
-    const apiary = await this.apiaryModel.find(
-      {
-        _id: apiaryId,
-      },
-      { tasks: 1 },
-    );
+    const apiary = await this.apiaryModel
+      .find(
+        {
+          _id: apiaryId,
+        },
+        { tasks: 1 },
+      )
+      .populate({ path: 'tasks.comments.userId' });
     return apiary;
   }
   async getCompletedTasks(apiaryId: string) {
@@ -79,21 +81,13 @@ export class TasksService {
     return updatedApiary;
   }
 
-  async completeTask(
-    apiaryId: string,
-    taskId: string,
-    updateTaskDto: UpdateTaskDto,
-  ) {
+  async completeTask(apiaryId: string, taskId: string) {
     // access a task in an apiary
     const updatedApiary = await this.apiaryModel.findOneAndUpdate(
       { _id: apiaryId, 'tasks._id': taskId },
       {
         $set: {
           'tasks.$.status': TaskStatus.Done, // Update the status of the matched task
-          'tasks.$.comment':
-            updateTaskDto.comment || updateTaskDto.comment == ''
-              ? 'No comment'
-              : updateTaskDto.comment,
         },
       },
       {
@@ -106,6 +100,28 @@ export class TasksService {
     return updatedApiary;
   }
 
+  async addComment(
+    apiaryId: string,
+    taskId: string,
+    updateTaskDto: UpdateTaskDto,
+  ) {
+    const newComment = new Comment(updateTaskDto);
+    // access a task in an apiary
+    const updatedApiary = await this.apiaryModel.findOneAndUpdate(
+      { _id: apiaryId, 'tasks._id': taskId },
+      {
+        $push: { 'tasks.$.comments': newComment },
+      },
+
+      {
+        new: true,
+        projection: { tasks: 1 }, //return task array only with apiary _id
+      },
+    );
+    // task or apiary not found
+    if (!updatedApiary) throw new NotFoundException('Task in apiary not found');
+    return updatedApiary;
+  }
   async deleteTask(apiaryId: string) {
     // find apiary with id and delete task with id
     const updatedApiary = await this.apiaryModel.updateMany(
