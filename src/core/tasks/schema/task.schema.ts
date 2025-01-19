7b9199a46eed9926bd048791731a0ab4199c1966
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-19 16:19:55 +0200

    Nestjs <> Add - comment schema and embed in task

--- src/core/tasks/schema/task.schema.ts
+++ src/core/tasks/schema/task.schema.ts
@@ -1,6 +1,26 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { ObjectId, Types } from 'mongoose';
+import mongoose, { ObjectId, Types } from 'mongoose';
+import { User } from 'src/core/users/schema/user.schema';
 import { TaskStatus } from 'src/utils/enums/taskStatus.enum';
+
+@Schema()
+export class Comment {
+  constructor({ userId, content }) {
+    this.content = content;
+    this.userId = userId;
+  }
+  _id: Types.ObjectId;
+
+  @Prop({ type: String, required: true })
+  content: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null })
+  userId: User;
+
+  @Prop({ type: Date, required: true, default: Date.now })
+  date: Date;
+}
+export const CommentSchema = SchemaFactory.createForClass(Comment);
 
 @Schema()
 export class Task {
@@ -19,8 +39,8 @@ export class Task {
   @Prop({ type: String, enum: TaskStatus, default: TaskStatus.Pending })
   status: string;
 
-  @Prop({ type: String, required: false, default: '' })
-  comment: string;
+  @Prop({ type: [CommentSchema], default: [] })
+  comments: Comment[];
 
   @Prop({ type: Date, required: true, default: Date.now })
   date: Date;
